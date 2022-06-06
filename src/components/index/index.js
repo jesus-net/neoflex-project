81edbf332b2846a10593ab140a72be6f06278fc7
Кристиан Принц <kristian.princz198101@gmail.com>
2022-06-06 09:21:40 +0400

    modified: adding pug mixins and positioning elements

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -1,1 +1,7 @@
 import '@components/index/index.scss';
+
+import '@components/input/input.js';
+import '@components/checkbox/checkbox.js';
+import '@components/button/button.js';
+import '@components/footer/footer.js';
+
