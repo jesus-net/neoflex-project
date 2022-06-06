62d277318214a5e1ea3187e19575cfca00e1787f
Кристиан Принц <kristian.princz198101@gmail.com>
2022-06-06 11:59:12 +0400

    modified and create: correction semantics, pixel perfect and add sign-up form

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -5,3 +5,15 @@ import '@components/checkbox/checkbox.js';
 import '@components/button/button.js';
 import '@components/footer/footer.js';
 
+const linkSignIn = document.querySelector('.sign-in__link');
+const linkSignUp = document.querySelector('.sign-up__link');
+
+const overlay = document.querySelector('.auth__overlay');
+
+linkSignUp.addEventListener('click', () => {
+	overlay.classList.remove("auth__overlay--active");
+});
+
+linkSignIn.addEventListener('click', () => {
+	overlay.classList.add("auth__overlay--active");
+});
