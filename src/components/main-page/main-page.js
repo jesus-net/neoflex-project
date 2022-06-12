25743b4f2c24ed5ddfc50ac3cf98258d86441ac4
Кристиан Принц <kristian.princz198101@gmail.com>
2022-06-12 11:11:05 +0400

    add: full page

--- src/components/main-page/main-page.js
+++ src/components/main-page/main-page.js
@@ -2,3 +2,5 @@ import '@components/main-page/main-page.scss';
 
 import '@components/navbar/navbar.js';
 import '@components/header/header.js';
+import '@components/button/button.js';
+import '@components/table/table.js';
