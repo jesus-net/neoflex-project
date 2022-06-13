71938e76325637c033faa896425d2f655969abfa
Кристиан Принц <kristian.princz198101@gmail.com>
2022-06-14 03:35:38 +0400

    modified: add adaptive

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,3 +1,19 @@
 import '@components/header/header.scss';
 
 import '@components/input/input.js';
+
+const menuBurger = document.querySelector('.header__burger');
+const navbar = document.querySelector('.navbar');
+const mainHome = document.querySelector('.home');
+
+menuBurger.addEventListener('click', function () {
+    navbar.classList.add('navbar--active');
+    menuBurger.classList.add('header__burger--disabled');
+});
+
+mainHome.addEventListener('click', function() {
+    navbar.classList.remove('navbar--active');
+    menuBurger.classList.remove('header__burger--disabled');
+});
+
+
