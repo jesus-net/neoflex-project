e5ca7a21e228040dffba5c7f31ab6020e44eeebf
Кристиан Принц <kristian.princz198101@gmail.com>
2022-07-07 01:26:16 +0400

    update: using object in hook

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,72 +1,50 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+
 import "@pages/Login/Login.scss";
-import Input from "@components/Input/Input";
-import Button from "@components/Button/Button";
-import Checkbox from "@components/Checkbox/Checkbox";
+
 import poster from "@img/poster-home-copy.png";
-import logoCompany from "@img/logo-company.svg";
+import LoginForm from "@components/LoginForm/LoginForm";
+
+import axios from "axios";
 
 const Login = () => {
-  const [stateOverlay, setStateOverlay] = useState("");
-  const [stateSignIn, setstateSignIn] = useState("");
-  const [stateSignUp, setstateSignUp] = useState("login__block--disabled");
+  const [stateForm, setStateForm] = useState({
+    overlay: "",
+    auth: "",
+    reg: "login__block--disabled",
+  });
 
-  const changeOverlay = (ref) => {
-    if (stateOverlay === "") {
-      setStateOverlay("login__overlay--active");
-      setstateSignIn("login__block--disabled");
-      setstateSignUp("");
-    } else {
-      setStateOverlay("");
-      setstateSignIn("");
-      setstateSignUp("login__block--disabled");
-    }
+  const handleOverlay = () => {
+    stateForm.overlay === ""
+      ? setStateForm({
+          overlay: "login__overlay--active",
+          auth: "login__block--disabled",
+          reg: "",
+        })
+      : setStateForm({
+          overlay: "",
+          auth: "",
+          reg: "login__block--disabled",
+        });
   };
 
+let DB = axios.create({
+  baseURL: 'http://localhost:3001',
+  headers: {Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYzFlY2U5ZDg1OGE1ZDI0NDYxM2I2NCIsImVtYWlsIjoidXNlcjExMUBtYWlsLnJ1IiwiaWF0IjoxNjU2ODc2MjY1LCJleHAiOjE2NTY5NjI2NjV9.ibt0FDMa-KlZkL_NthxDydBCmsEuqSjtsCRYhJB4htA`}
+});
+
+DB.get('/user').then(res => console.log(res.data.users));
   return (
     <main className="login">
       <div className="login__container">
-        <figure className={"login__overlay " + stateOverlay}>
+        <figure className={"login__overlay " + stateForm.overlay}>
           <img src={poster} alt="login poster"></img>
         </figure>
-        <section className={"login__block sign-up " + stateSignUp}>
-          <img src={logoCompany} alt="logo company"></img>
-          <form className="login__form">
-            <Input
-              type="textfield"
-              label="full name"
-              placeholder="Type your full name"
-            />
-            <Input type="email" label="e-email" />
-            <Input type="password" label="password" />
-            <Button type="submit" value="Login" />
-            <p className="login__link">
-              Have an account?
-              <a href="#" onClick={() => changeOverlay("sign-in")}>
-                {" "}
-                Request authorization
-              </a>
-            </p>
-          </form>
+        <section className={"login__block " + stateForm.reg}>
+          <LoginForm type="reg" clickLink={handleOverlay} />
         </section>
-        <section className={"login__block sign-in " + stateSignIn}>
-          <img src={logoCompany} alt="logo company"></img>
-          <form className="login__form" action="#">
-            <Input type="email" label="e-email" />
-            <Input type="password" label="password" />
-            <Checkbox></Checkbox>
-            <Link to="/">
-              <Button type="submit" value="Login" />
-            </Link>
-            <p className="login__link">
-              Not a member?
-              <a href="#" onClick={() => changeOverlay("sign-up")}>
-                {" "}
-                Request registration
-              </a>
-            </p>
-          </form>
+        <section className={"login__block " + stateForm.auth}>
+          <LoginForm type="auth" clickLink={handleOverlay} />
         </section>
       </div>
     </main>
@@ -74,3 +52,4 @@ const Login = () => {
 };
 
 export default Login;
+
