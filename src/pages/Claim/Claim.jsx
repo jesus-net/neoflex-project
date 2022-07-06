ade4918bab85f76e815071c3bf9f54710820780a
Кристиан Принц <kristian.princz198101@gmail.com>
2022-07-07 01:12:32 +0400

    update: use ClaimForm and transfer styles

--- src/pages/Claim/Claim.jsx
+++ src/pages/Claim/Claim.jsx
@@ -1,20 +1,13 @@
 import "@pages/Claim/Claim.scss";
-import Input from "@components/Input/Input";
-import Button from "@components/Button/Button";
-const Claim = () => {
+
+import ClaimForm from "@components/ClaimForm/ClaimForm";
+
+const Claim = ({onClickMain}) => {
     return (
-         <main className="claim">
+         <main className="claim" onClick={onClickMain}>
          <div className="claim__container">
             <h1 className="title">Creating new claim</h1>
-            <form className="claim__form" action="">
-            <Input type="textfield" label="title"/>
-            <Input type="dropdown" label="type"/>
-            <Input type="textfield" label="description" placeholder="Type claim description"/>
-            <div className="claim__buttons">
-            <Button type="cancel"/>
-            <Button type="submit"/>
-            </div>
-            </form>
+            <ClaimForm/>
          </div>
        </main>
     )
