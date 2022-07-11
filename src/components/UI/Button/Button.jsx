5167374899558ea275445a4552c6993b225344be
Кристиан Принц <kristian.princz198101@gmail.com>
2022-07-11 16:17:45 +0400

    add: props

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,23 +1,31 @@
 import "@UI/Button/Button.scss";
 import iconPlus from "@img/icon-plus.svg";
-const Button = ({ type, value, onClick }) => {
+const Button = ({ type, value, disabled, onClick }) => {
   switch (type) {
     case "submit":
       return (
-        <button className="button submit" type="submit" onClick={onClick}>
+        <button
+          className="button submit"
+          type="submit"
+          disabled={disabled}
+          onClick={(e) => {
+            onClick();
+            e.preventDefault();
+          }}
+        >
           {value || "Create"}
         </button>
       );
-    case "plus": 
-        return (
-          <button className="button plus" type="submit">
+    case "plus":
+      return (
+        <button className="button plus" disabled={disabled} type="submit">
           <img src={iconPlus} alt="plus" />
           {value || "Create claim"}
         </button>
-        )
+      );
     case "reset":
       return (
-        <button className="button reset" type="reset">
+        <button className="button reset" disabled={disabled} type="reset">
           {value || "Decline"}
         </button>
       );
