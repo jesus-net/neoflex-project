b31c9a5d46878f61e8a87ead300973a3c926b758
Кристиан Принц <kristian.princz198101@gmail.com>
2022-07-11 16:21:41 +0400

    add: prop onSelect for dropdown and bind value validation in dropdown

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -7,7 +7,16 @@ import iconPassword from "@img/icon-password.svg";
 import iconSearch from "@img/icon-search.svg";
 import iconDropdown from "@img/icon-dropdown.svg";
 
-const Input = ({ type, label, placeholder}) => {
+const Input = ({
+  type,
+  label,
+  maxLength,
+  placeholder,
+  value,
+  onChange,
+  onBlur,
+  onSelect
+}) => {
   let input;
   const typeDropdown = [
     "Hardware",
@@ -15,69 +24,8 @@ const Input = ({ type, label, placeholder}) => {
     "Troubleshooting",
     "Networking",
   ];
+  
   const [isActiveDropdown, setActiveDropdown] = useState(false);
-  const [valueDropdown, setValueDropdown] = useState(undefined);
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [fullName, setFullName] = useState("");
-
-  const [emailDirty, setEmailDirty] = useState(false);
-  const [passwordDirty, setPasswordDirty] = useState(false);
-  const [fullNameDirty, setFullNameDirty] = useState(false);
-
-  const [emailErr, setEmailErr] = useState("Wrong email");
-  const [passwordErr, setPasswordErr] = useState("Wrong password");
-  const [fullNameErr, setFullNameErr] = useState("Wrong full name");
-
-  const blurHandler = (e) => {
-    switch (e.target.name) {
-      case "email":
-        setEmailDirty(true);
-        break;
-      case "password":
-        setPasswordDirty(true);
-        break;
-      case "full name":
-        setFullNameDirty(true);
-        break;
-    }
-  };
-
-  const emailHandler = (e) => {
-    setEmail(e.target.value);
-    const re =
-      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if (e.target.value.length === 0) {
-      setEmailErr("Wrong email");
-    } else if (!re.test(String(e.target.value).toLowerCase()))
-      setEmailErr("Incorrect email");
-    else setEmailErr("");
-  };
-
-  const passwordHandler = (e) => {
-    setPassword(e.target.value);
-
-    if (e.target.value.length === 0) setPasswordErr("Wrong password");
-    else if (e.target.value[0] === e.target.value[0].toLowerCase())
-      setPasswordErr("Password must start with a capital letter");
-    else if (e.target.value.length < 6)
-      setPasswordErr("Password must be more than 6 characters");
-    else {
-      setPasswordErr("");
-    }
-  };
-
-  const fullNameHandler = (e) => {
-    setFullName(e.target.value);
-    const re = /[a-zа-я]/i;
-    if (e.target.value.length === 0) setFullNameErr("Wrong full name");
-    else if (!re.test(String(e.target.value).toLowerCase()))
-      setFullNameErr("Full name must be letters");
-    else if (e.target.value.length < 4) {
-      setFullNameErr("Full name must be more than 4 characters");
-    } else setFullNameErr("");
-  };
 
   switch (type) {
     case "email":
@@ -85,37 +33,33 @@ const Input = ({ type, label, placeholder}) => {
         <>
           <input
             className="input__field"
-            type={type}
+            type="email"
+            value={value}
             name={type}
-            value={email}
+            maxLength={maxLength}
             placeholder={placeholder || "Type your e-mail"}
-            onBlur={(e) => blurHandler(e)}
-            onChange={(e) => emailHandler(e)}
+            onChange={(e) => onChange(e)}
+            onBlur={(e) => onBlur(e)}
           />
           <img src={iconEmail}></img>
-          {emailDirty && emailErr && (
-            <div className="input__error">{emailErr}</div>
-          )}
         </>
       );
       break;
     case "password":
       input = (
-        <>
+        <div className="">
           <input
             className="input__field"
             type="password"
+            value={value}
             name={type}
-            value={password}
+            maxLength={maxLength}
             placeholder={placeholder || "Type your password"}
-            onBlur={(e) => blurHandler(e)}
-            onChange={(e) => passwordHandler(e)}
+            onChange={(e) => onChange(e)}
+            onBlur={(e) => onBlur(e)}
           />
           <img src={iconPassword}></img>
-          {passwordDirty && passwordErr && (
-            <div className="input__error">{passwordErr}</div>
-          )}
-        </>
+        </div>
       );
       break;
     case "textfield":
@@ -125,14 +69,11 @@ const Input = ({ type, label, placeholder}) => {
             className="input__field"
             type="text"
             name={label}
-            value={fullName}
+            maxLength={maxLength}
             placeholder={placeholder || "Type claim title"}
-            onBlur={(e) => blurHandler(e)}
-            onChange={(e) => fullNameHandler(e)}
+            onChange={(e) => onChange(e)}
+            onBlur={(e) => onBlur(e)}
           />
-          {fullNameDirty && fullNameErr && (
-            <div className="input__error">{fullNameErr}</div>
-          )}
         </>
       );
       break;
@@ -143,6 +84,7 @@ const Input = ({ type, label, placeholder}) => {
             className="input__field"
             type="text"
             name={type}
+            maxLength={30}
             placeholder={placeholder || "Search"}
           />
           <img src={iconSearch}></img>
@@ -153,13 +95,15 @@ const Input = ({ type, label, placeholder}) => {
     case "dropdown":
       input = (
         <>
-          {valueDropdown && <div className={"type " + valueDropdown}></div>}
+          {value && <div className={"type " + value}></div>}
           <input
             className={
-              "input__field" + (valueDropdown ? " input__field--active" : "")
+              "input__field" + (value ? " input__field--active" : "")
             }
-            placeholder={placeholder || valueDropdown || "Select type"}
+            placeholder={placeholder || "Select type"}
+            value={value}
             onClick={() => setActiveDropdown(!isActiveDropdown)}
+            onBlur={(e) => onBlur(e)}
             readOnly
           />
           <img
@@ -178,7 +122,7 @@ const Input = ({ type, label, placeholder}) => {
             {typeDropdown.map((item) => (
               <span
                 className={"dropdown___select " + item}
-                onClick={() => setValueDropdown(item)}
+                onClick={() => onSelect(item)}
               >
                 {item}
               </span>
@@ -186,6 +130,7 @@ const Input = ({ type, label, placeholder}) => {
           </div>
         </>
       );
+      break;
   }
   return (
     <div className={"input " + type}>
