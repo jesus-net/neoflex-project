cfb1d2c1a4bc14a42c973549e1f07c926be18aaa
Кристиан Принц <kristian.princz198101@gmail.com>
2022-07-18 19:37:22 +0400

    add: selector search value and nickname

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,42 +1,68 @@
-import React from "react"; 
-import {Link} from "react-router-dom";
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
 import "@components/Header/Header.scss";
 import Input from "@UI/Input/Input";
 import iconBell from "@img/icon-notification.svg";
 import picAvatar from "@img/avatar.jpg";
 import iconExit from "@img/icon-exit.svg";
+import { useDispatch, useSelector } from "react-redux";
+import { setToken } from "@store/userSlice";
+import { toggleNavbar } from "@store/homeSlice";
+import {getClaims} from "@store/action.homeSlice";
 
+const Header = ({ isSearch = true }) => {
+  const dispatch = useDispatch();
+  const [searchValue, setSerchValue] = useState('');
 
-const Header = ({onClickMenu, isSearch = true}) => (
-  <header className="header">
-    <div className="header__container">
-      <nav className="header__nav">
-        <div className="header__burger" onClick = {onClickMenu}>
-          <span></span>
-        </div>
-        <div className="header__search">
-          {isSearch&&<Input type="search" />}
-        </div>
-        <div className="header__overlay">
-          <a
-            href="#"
-            className="header__notification header__notification--active"
+  const searchHandler = (e) =>  {
+    setSerchValue(e.target.value);
+    dispatch(getClaims(searchValue));
+  }	
+
+  const nickName = useSelector((state) => state.user.fullName);
+  console.log(nickName);
+  return (
+    <header className="header">
+      <div className="header__container">
+        <nav className="header__nav">
+          <div
+            className="header__burger"
+            onClick={() => dispatch(toggleNavbar())}
           >
-            <img src={iconBell} alt="bell" />
-          </a>
-          <div className="header__avatar">
-            <img src={picAvatar} alt="avatar" />
+            <span></span>
           </div>
-          <p className="header__nickname">Ivan Ivanov</p>
-          <Link to='/login'>
-          <div className="header__exit">
-            <img src={iconExit} alt="exit" />
+          <div className="header__search">
+            {isSearch && <Input type="search" onChange={searchHandler} />}
           </div>
-          </Link>
-        </div>
-      </nav>
-    </div>
-  </header>
-);
+          <div className="header__overlay">
+            <a
+              href="#"
+              className="header__notification header__notification--active"
+            >
+              <img src={iconBell} alt="bell" />
+            </a>
+            <div className="header__avatar">
+              <img src={picAvatar} alt="avatar" />
+            </div>
+            <p className="header__nickname">{nickName}</p>
+            <Link to="/">
+              <div
+                className="header__exit"
+                onClick={() => dispatch(
+                  setToken({
+                    token: "",
+                    fullName: "",
+                  })
+                )}
+              >
+                <img src={iconExit} alt="exit" />
+              </div>
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
