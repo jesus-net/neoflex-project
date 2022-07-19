64e73d4a498f9d16aea8e0790cf957b3edabf57f
Кристиан Принц <kristian.princz198101@gmail.com>
2022-07-20 01:37:13 +0400

    add: new Pagination in page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,25 +3,26 @@ import { Link } from "react-router-dom";
 import "@pages/Home/Home.scss";
 import Button from "@UI/Button/Button";
 import ClaimList from "@components/Claim-list/Claim-list";
-import Pagination from "@components/Pagination/Pagination";
-import {useDispatch} from "react-redux";
+import PaginationList from "@components/Pagination/Pagination";
+import { useDispatch } from "react-redux";
 import { toggleNavbar } from "@slice/homeSlice";
+
 const Home = () => {
   const dispatch = useDispatch();
   const handleHome = () => dispatch(toggleNavbar(false));
   return (
-      <main className="home" onClick={handleHome}>
-        <div className="home__container">
-          <h1 className="title">Your claims</h1>
-          <Link to='/claim'>
+    <main className="home" onClick={handleHome}>
+      <div className="home__container">
+        <h1 className="title">Your claims</h1>
+        <Link to="/claim">
           <div className="home__button">
             <Button type="plus" value="Create claim" />
           </div>
-          </Link>
-          <ClaimList />
-          <Pagination/>
-        </div>
-      </main>
+        </Link>
+        <ClaimList />
+        <PaginationList/>
+      </div>
+    </main>
   );
 };
 
