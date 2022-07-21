75aeaf263d56b06c65d1b26c5b0ec02bdb11bb71
Кристиан Принц <kristian.princz198101@gmail.com>
2022-07-21 04:19:27 +0400

    fix: rename className and add name attributes

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,16 +1,17 @@
 import "@UI/Button/Button.scss";
 import iconPlus from "@img/icon-plus.svg";
 
-const Button = ({ type, value, onClick }) => {
+const Button = ({ type, value, onClick, name }) => {
   switch (type) {
     case "submit":
       return (
         <button
-          className="button submit "
+          className="button submit"
           type="submit"
+          name={name}
           onClick={(e) => {
             e.preventDefault();
-            onClick();
+            onClick(e);
           }}
         >
           {value || "Create"}
@@ -18,19 +19,46 @@ const Button = ({ type, value, onClick }) => {
       );
     case "plus":
       return (
-        <button className="button plus" type="submit">
+        <button
+          className="button plus"
+          type="submit"
+          name={name}
+          onClick={(e) => {
+            e.preventDefault();
+            onClick(e);
+          }}
+        >
           <img src={iconPlus} alt="plus" />
           {value || "Create claim"}
         </button>
       );
-    case "reset":
+    case "declined":
       return (
-        <button className="button reset" type="reset">
+        <button
+          className="button reset"
+          type="submit"
+          name={name}
+          onClick={(e) => {
+            e.preventDefault();
+            onClick(e);
+          }}
+        >
           {value || "Decline"}
         </button>
       );
     case "cancel":
-      return <button className="button cancel">{value || "Cancel"}</button>;
+      return (
+        <button
+          type="submit"
+          className="button cancel"
+          name={name}
+          onClick={(e) => {
+            e.preventDefault();
+            onClick(e)}}
+        >
+          {value || "Cancel"}
+        </button>
+      );
   }
 };
 
