fe2f06735beeecbbd066191aeed91000454eb496
Кристиан Принц <kristian.princz198101@gmail.com>
2022-07-21 04:13:23 +0400

    add: id for button Browse

--- src/components/Claim-list/Claim/Claim.jsx
+++ src/components/Claim-list/Claim/Claim.jsx
@@ -1,13 +1,6 @@
 import "@components/Claim-list/Claim/Claim.scss";
-
-const Claim = ({
-  title,
-  created,
-  type,
-  typeSlug,
-  status, 
-  statusSlug 
-}) => {
+import { Link } from "react-router-dom";
+const Claim = ({ id, title, created, type, typeSlug, status, statusSlug }) => {
   return (
     <div className="claim">
       <div className="claim__title">
@@ -17,7 +10,7 @@ const Claim = ({
       <div className="claim__block">
         <div className="claim__filter">Created</div>
         <div className="claim__created">
-        <div className="claim__text">{created}</div>
+          <div className="claim__text">{created}</div>
         </div>
       </div>
       <div className="claim__block">
@@ -30,8 +23,9 @@ const Claim = ({
         <div className="claim__filter">Status</div>
         <div className={"claim__status " + statusSlug}>{status}</div>
       </div>
-
-      <button /* onClick={onClick} */ className="claim__button">Browse</button>
+      <button className="claim__button">
+        <Link to={`/post${id}`}>Browse</Link>
+      </button>
     </div>
   );
 };
