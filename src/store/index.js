38a4f251ce89da077c348560965ef4e892bc6b47
Кристиан Принц <kristian.princz198101@gmail.com>
2022-07-21 05:13:35 +0400

    add: claim reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "@slice/userSlice";
 import homeReducer from "@slice/homeSlice";
-
+import claimReducer from "@slice/claimSlice";
 export default configureStore({
   reducer: {
     user: userReducer,
-    home: homeReducer
+    home: homeReducer,
+    claim: claimReducer
   },
   middleware: (getDefaultMiddleware) =>
   getDefaultMiddleware({
